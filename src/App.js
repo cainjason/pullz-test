c8afbe0f2add09594ba4bcbc61557244d7521462
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-08-06 10:16:12 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Pullup World!</h1>
+      <button className="btn btn-outline">Default</button>
+      <button className="btn btn-outline btn-primary">Primary</button>
+      <button className="btn btn-outline btn-secondary">Secondary</button>
+      <button className="btn btn-outline btn-accent">Accent</button>
+    </div>
+  );
+}
+
+export default App;
